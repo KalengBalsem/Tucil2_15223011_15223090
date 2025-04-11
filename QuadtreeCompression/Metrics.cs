f800394b35271a6fab48faed93cb2d61d5031b00
AmIARealDeveloper <ignacioalberiann@gmail.com>
2025-04-11 08:24:48 +0700

    feat: addition of MAD error

--- QuadtreeCompression/Metrics.cs
+++ QuadtreeCompression/Metrics.cs
@@ -41,6 +41,38 @@ class Variance
 
 class MAD()  // Mean Absolute Deviation
 {
+    public static double CalculateError(byte[,,] pixelMatrix, int y, int x, int height, int width)
+    {
+        double sumR = 0, sumG = 0, sumB = 0;
+        int numPixels = height * width;
+        for (int i = y; i < y + height; i++)
+        {
+            for (int j = x; j < x + width; j++)
+            {
+                sumR += pixelMatrix[i, j, 0];
+                sumG += pixelMatrix[i, j, 1];
+                sumB += pixelMatrix[i, j, 2];
+            }
+        }
+        int avgR = (int)(sumR / numPixels);
+        int avgG = (int)(sumG / numPixels);
+        int avgB = (int)(sumB / numPixels);
+
+        double varR = 0, varG = 0, varB = 0;
+        for (int i = y; i < y + height; i++)
+        {
+            for (int j = x; j < x + width; j++)
+            {
+                varR += Math.Abs(pixelMatrix[i, j, 0] - avgR);
+                varG += Math.Abs(pixelMatrix[i, j, 1] - avgG);
+                varB += Math.Abs(pixelMatrix[i, j, 2] - avgB);
+            }
+        }
+        varR /= numPixels;
+        varG /= numPixels;
+        varB /= numPixels;
+        return varR + varG + varB;
+    }
 }
 
 class MaxPixelDifference()
