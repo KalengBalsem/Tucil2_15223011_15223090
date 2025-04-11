b6228d40a7e1f02d0c3ed681d32639eb03942ec2
AmIARealDeveloper <ignacioalberiann@gmail.com>
2025-04-11 15:14:41 +0700

    feat: SSIM and connecting Quadtree

--- QuadtreeCompression/Metrics.cs
+++ QuadtreeCompression/Metrics.cs
@@ -2,8 +2,15 @@ using System.Runtime.InteropServices;
 
 namespace QuadtreeCompression;
 
+// DARI PROGRAM MEMILIH METODE YANG DIGUNAKAN
+public interface errorMethod
+{
+    double CalculateError(byte[,,] pixelMatrix, int y, int x, int height, int width);
+}
+
+
 // ERROR MEASUREMENT METHOD
-class Variance
+class Variance : errorMethod
 {
     public static double CalculateError(byte[,,] pixelMatrix, int y, int x, int height, int width)
     {
@@ -41,7 +48,7 @@ class Variance
     }
 }
 
-class MAD  // Mean Absolute Deviation
+class MAD : errorMethod // Mean Absolute Deviation
 {
     public static double CalculateError(byte[,,] pixelMatrix, int y, int x, int height, int width)
     {
@@ -79,7 +86,7 @@ class MAD  // Mean Absolute Deviation
     }
 }
 
-class MaxPixelDifference
+class MaxPixelDifference : errorMethod 
 {
     public static double CalculateError(byte[,,] pixelMatrix, int y, int x, int height, int width)
     {
@@ -113,7 +120,7 @@ class MaxPixelDifference
     }
 }
 
-class Entropy
+class Entropy : errorMethod
 {
     public static double CalculateError(byte[,,] pixelMatrix, int y, int x, int height, int width)
     {
@@ -155,6 +162,90 @@ class Entropy
 
 }
 
-class SSIM  // Structural Similarity Index
+class SSIM  : errorMethod // Structural Similarity Index
 {
+    public static double CalculateError(byte[,,] pixelMatrix, int y, int x, int height, int width)
+    {
+        // Menghitung C1-C2 berdasarkan rumus dari SSIM (Wang et al., 2004) dengan nilai piksel 255 untuk 8-bit gambar
+        double L = 255; // 8-bit 
+        double K1 = 0.01;
+        double K2 = 0.03;
+        double C1 = K1*L*K1*L; // C1 = (K1*L)^2 = 6.5025
+        double C2 = K2*L*K2*L; // C2 = (K2*L)^2 = 58.5225
+
+        double muX_R = 0, muX_G = 0, muX_B = 0;
+        int numPixels = height * width;
+
+        //hitung mean dari gambar blok asli
+        for (int i = y; i < y + height; i++)
+        {
+            for (int j = x; j < x + width; j++)
+            {
+                muX_R += originalMatrix[i, j, 0];
+                muX_G += originalMatrix[i, j, 1];
+                muX_B += originalMatrix[i, j, 2];
+            }
+        }
+
+        muX_R /= numPixels;
+        muX_G /= numPixels;
+        muX_B /= numPixels;
+
+        // Tidak ada Blok Hasil Rekonstruksi, Simulasi Blok Rekosntruksi ketika sedang dalam proses kompresi
+        double muY_R = muX_R;
+        double muY_G = muX_G;
+        double muY_B = muX_B;
+
+        //VARIANCE AND COVARIANCE
+        double varX_R = 0, varX_G = 0, varX_B = 0;
+        double varY_R = 0, varY_G = 0, varY_B = 0;
+        double covXY_R = 0, covXY_G = 0, covXY_B = 0;
+
+        for (int i = y; i < y + height; i++)
+        {
+            for (int j = x; j < x + width; j++)
+            {
+                double oxR = pixelMatrix[i, j, 0] - muX_R;
+                double oxG = pixelMatrix[i, j, 1] - muX_G;
+                double oxB = pixelMatrix[i, j, 2] - muX_B;
+
+                // blok rekonstruksi konstan, deviasi = 0, maka varY = 0, dan covXY = 0
+                varX_R += oxR * oxR;
+                varX_G += oxG * oxG;
+                varX_B += oxB * oxB;
+            }
+        }  
+
+        varX_R /= (numPixels - 1); 
+        varX_G /= (numPixels - 1);
+        varX_B /= (numPixels - 1);
+
+
+        varY_R = 0;
+        varY_G = 0;
+        varY_B = 0;
+        covXY_R = 0;
+        covXY_G = 0;
+        covXY_B = 0;
+
+        // Hitung SSIM per Kanal warna
+        double numerator_R = ((2 * muX_R * muY_R + C1) * (2 * covXY_R + C2));
+        double denomerator_R = ((muX_R * muX_R + muY_R * muY_R + C1) * (varX_R + varY_R + C2));
+        double ssim_R = numerator_R / denomerator_R;
+
+        double numerator_G = ((2 * muX_G * muY_G + C1) * (2 * covXY_G + C2));
+        double denomerator_G = ((muX_G * muX_G + muY_G * muY_G + C1) * (varX_G + varY_G + C2));
+        double ssim_G = numerator_G / denomerator_G;
+
+        double numerator_B = ((2 * muX_B * muY_B + C1) * (2 * covXY_B + C2));
+        double denomerator_B = ((muX_B * muX_B + muY_B * muY_B) * (varX_B + varY_B + C2));
+        double ssim_B = numerator_B / denomerator_B;
+
+        // SSIM VALUE
+        ssim_R /= numPixels
+        ssim_G /= numPixels
+        ssim_B /= numPixels
+
+        return (1.0 - ssim_R) + (1.0 - ssim_G) + (1.0 - ssim_B);
+    }
 }
