54997fa60c52f3f95457116e21bf5915474c26d3
KalengBalsem <balsemcode@gmail.com>
2025-04-11 21:56:22 +0700

    fix(quadtree): fix SSIM bug

--- QuadtreeCompression/obj/Debug/net9.0/QuadtreeCompression.AssemblyInfo.cs
+++ QuadtreeCompression/obj/Debug/net9.0/QuadtreeCompression.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuadtreeCompression")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20a31502e7299348c2a90324a46366275073cbe3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cf18ebbe7f5e5819a32e0625ea0842a22ca3071")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuadtreeCompression")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuadtreeCompression")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
